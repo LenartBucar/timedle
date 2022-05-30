d13eeb4c1f8b20d585ffc7c88f6d3df42c149884
LenartBucar <lenart.bucar@student.fmf.uni-lj.si>
2022-05-30 19:03:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Timedle
+
+Project for Programming 2 class on FMF UNI-LJ.
+
+## Authors
+
+[Lenart Bučar](https://github.com/LenartBucar)
+
+[Maša Žaucer](https://github.com/masazaucer)
---
+++ src/timedle/Timedle.java
@@ -0,0 +1,8 @@
+package timedle;
+
+public class Timedle {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test message. Please remain calm. ");
+    }
+}
